7ac6dc1b3e4354873c0c21310b096e8ab6b90806
Ivan <vanzup@mail.ru>
2024-10-21 22:15:34 +0300

    edit workflow test 5

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '130.193.52.17', 'kyttygram.sytes.net']
 
